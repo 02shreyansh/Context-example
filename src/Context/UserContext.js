d08a5238892bf172b03d2655aa2c3f327cb71aeb
SHREYANSH <137887705+02shreyansh@users.noreply.github.com>
2024-09-14 22:58:44 +0530

    Update UserContext.js

--- src/Context/UserContext.js
+++ src/Context/UserContext.js
@@ -1,3 +1,9 @@
-import React from "react";
-const UserContext=React.createContext()
-export default UserContext;
+import { createContext, useContext } from "react";
+export const UserContext=createContext({
+    user:"",
+    setUserr:()=>{}
+})
+export const UserContextProvider=UserContext.Provider
+export default function useUser(){
+    return useContext(UserContext)
+}
